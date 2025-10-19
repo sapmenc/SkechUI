d107e04adc121bb51fc70e016ca160f50d212a89
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-10-19 14:00:15 +0530

    commit

--- sketch-ui/src/app/auth/sign-up/page.tsx
+++ sketch-ui/src/app/auth/sign-up/page.tsx
@@ -7,7 +7,6 @@ import Link from 'next/link'
 export default function SignupPage() {
 
 
-
     return (
         <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
             <form
